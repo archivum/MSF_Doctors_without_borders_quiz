6d3e58d182a0707f4ecc9b05f651ea3d4888ef12
alphieeee <alphie@candybanners.com>
2018-07-19 00:37:28 +0800

    removed parent component global css v2

--- src/components/Quiz.vue
+++ src/components/Quiz.vue
@@ -4,10 +4,9 @@
       <div class="full-width-container container">
         <!-- QUIZ SECTION -->
         <div v-for="(question, index) in quiz.questions" :key="index">
-          <div v-show="index === questionIndex" v-bind:style="{ 'background-image': 'url(' + question.images + ')' }" class="row full-bg ">
+          <div v-show="index === questionIndex" v-bind:style="{ 'background-image': 'url(' + question.images + ')' }" class="row full-bg">
             
             <div class="eight columns offset-by-two quiz">
-<!--              <div id='bg-img'><img :src="question.images"/></div>-->
               <div class='quizLogo'><img :src="quiz.logo"/></div>
               <h3 class="question">{{ question.text }}</h3>
               <ul class="questions-input">
@@ -230,35 +229,90 @@
 </script>
 
 <style scoped>
+/*Links*/
+a {
+    color: white !important;
+    text-decoration: none;
+    font-family: 'FreightSans Pro';
+    font-weight: 500;
+    font-style: normal;
+    cursor: pointer;
+}
+    
+/*Full width container*/
+.full-width-container {
+    height: 100%;
+    width: 100%;
+    max-width: 100%;
+    padding: 0px 0px;
+    clear: both;
+    margin: 0 auto;
+}
+    
+.row {
+    padding-left: 20px;
+    padding-right: 20px;
+}
 
-  /*Quiz*/
-
-  .fade-enter-active {
+.row-v-align {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+}
+    
+/*Quiz*/
+.fade-enter-active {
     transition: opacity .5s;
-  }
+}
 
-  .fade-enter {
+.fade-enter {
     opacity: 0;
-  }
+}
 
-  #app {
+#app {
     min-height: 100vh;
     background-color: rgba(0, 0, 0, 0.6);
-  }
+}
 
-  h1 {
+h1 {
     margin-bottom: 20px;
-  }
+}
 
-  ul {
+ul {
     list-style-type: none;
-  }
+}
 
-  button {
-    float: left;
-  }
-
-  .button-back {
+/*Button styles*/
+button {
+    height: 55px;
+    background-color: #ea0029;
+    border-radius: 50px;
+    border: none;
+    width: 275px;
+    font-size: 2.6rem;
+    color: white;
+    display: inline-block;
+    vertical-align: middle;
+    -webkit-transform: perspective(1px) translateZ(0);
+    transform: perspective(1px) translateZ(0);
+    box-shadow: 0 0 1px rgba(0, 0, 0, 0);
+    overflow: hidden;
+    -webkit-transition-duration: 0.3s;
+    transition-duration: 0.3s;
+    -webkit-transition-property: color, background-color;
+    transition-property: color, background-color;
+    font-family: 'FreightSans Pro';
+    font-weight: 600;
+    font-style: normal;
+}
+    
+button:hover, button:focus, button:active {
+    background-color: #b11515;
+    color: white;
+}
+    
+.button-back {
     color: white;
     float: left;
     margin-top: 10px;
@@ -266,29 +320,28 @@
     font-family: 'FreightSans Pro';
     font-weight: 500;
     font-style: normal;
-  }
+}
 
-  [v-cloak] {
+[v-cloak] {
     display: none;
-  }
+}
 
-  /*answer boxes*/
-
-  input[type="radio"] {
+/*answer boxes*/
+input[type="radio"] {
     display: none;
-  }
+}
 
-  .quiz {
+.quiz {
     padding-top: 150px;
-  }
+}
 
-  .quizLogo {
-/*    position: absolute;*/
+.quizLogo {
+    /*    position: absolute;*/
     float: left;
     margin-bottom: 50px;
-  }
+}
 
-  .question {
+.question {
     clear: left;
     text-align: left;
     margin-left: 20px;
@@ -300,9 +353,9 @@
     font-weight: bold;
     font-style: normal;
     font-size: 3.4rem;
-  }
+}
 
-  .questions-input {
+.questions-input {
     display: flex;
     -webkit-flex-direction: row;
     -moz-flex-direction: row;
@@ -328,18 +381,17 @@
     -o-align-items: stretch;
     -khtml-align-items: stretch;
     align-items: stretch;
-  }
+}
 
-  .questions-input li {
+.questions-input li {
     width: 48%;
-  }
+}
 
-  .inputs {
+.inputs {
     float: left;
-  }
+}
 
-  .answer {
-
+.answer {
     display: flex;
     align-items: center;
     justify-content: center;
@@ -359,9 +411,9 @@
     transition-duration: 0.3s;
     -webkit-transition-property: color, background-color;
     transition-property: color, background-color;
-  }
+}
 
-  .answer p {
+.answer p {
     margin-bottom: 0;
     color: #630808;
     padding-right: 20px;
@@ -369,169 +421,151 @@
     font-family: 'FreightSans Pro';
     font-weight: 500;
     font-style: normal;
-  }
+}
 
-  .answer:hover {
+.answer:hover {
     background: white;
     opacity: 1;
-  }
+}
 
-  .answer:active {
+.answer:active {
     background-color: #ea0029;
     color: white;
-  }
+}
 
-  .progress-and-button {
+.progress-and-button {
     display: inline-block;
     float: left;
-  }
+}
 
-  .progress-container {
+.progress-container {
     margin-top: 20px;
     margin-left: 20px;
     position: relative;
     float: left;
-  }
+}
 
-  .progress {
+.progress {
     z-index: 3;
     position: absolute;
     height: 5px;
     background-color: #ea0029;
     float: left;
-  }
+}
 
-  .progress-bar {
+.progress-bar {
     height: 5px;
     width: 250px;
     background-color: white;
     float: left;
-  }
+}
 
-  .full-bg {
+.full-bg {
     background: url("../../static/img/form.jpg") center center;
     height: 100vh;
     background-size: cover;
     background-repeat:  no-repeat;
     background-attachment: fixed;
-  }
+}
 
 
-  #bg-img {
-/*    background: url("../../static/img/form.jpg") center center;*/
+#bg-img {
+    /*    background: url("../../static/img/form.jpg") center center;*/
     height: 100%;
     background-size: cover;
     background-position: center center;
     background-repeat:  no-repeat;
     background-attachment: fixed;
-/*     z-index: -1; */
+    /*     z-index: -1; */
     position: fixed;
     top: 0;
     left: 0;
-  }
-/*
-    .bg-img{
-        background: url("../../static/img/form.jpg") center center;
-    height: 100vh;
-    background-size: cover;
-    background-repeat:  no-repeat;
-    background-attachment: fixed;
-    }
-*/
-  /*Quiz - End*/
+}
 
-  /*Results almost in*/
-
-  .results {
+/*Results almost in*/
+.results {
     color: white;
     text-align: left;
-  }
+}
 
 
-  .results-flex {
+.results-flex {
     display: flex;
     align-items: flex-start;
     justify-content: center;
-  }
+}
 
-  .results h3 {
+.results h3 {
     font-size: 3rem;
     font-weight: bold;
     font-family: 'FreightSans Pro';
     font-weight: bold;
     font-style: normal;
-  }
+}
 
-  .results p {
+.results p {
     font-size: 2rem;
     font-family: 'FreightSans Pro';
     font-weight: 500;
     font-style: normal;
-  }
+}
 
-  .results .u-full-width {
+.results .u-full-width {
     height: 50px;
     border-radius: 0px;
     margin-bottom: 5px;
     font-family: 'FreightSans Pro';
     font-weight: 500;
     font-style: normal;
-  }
+}
 
-  input[type="checkbox"] {
+input[type="checkbox"] {
     margin-top: 20px;
-  }
+}
 
-  .results .skip {
+.results .skip {
     clear: left;
     float: left;
     font-weight: 500;
     font-size: 1.6rem;
     padding-left: 82px;
     text-transform: uppercase;
-  }
+}
 
-  label > .label-body {
+label > .label-body {
     display: inline;
     font-family: 'FreightSans Pro';
     font-weight: 500;
     font-style: normal;
-  }
+}
 
-  .agree {
+.agree {
     margin-bottom: 40px;
-  }
+}
 
-  .agree input {
+.agree input {
     margin-bottom: 0;
-  }
+}
 
-  .response-selected {
+.response-selected {
     background-color: #ea0029;
     opacity: 1;
     color: white;
-  }
+}
 
-  a {
-    font-family: 'FreightSans Pro';
-    font-weight: 500;
-    font-style: normal;
-  }
-
-  @media only screen and (max-width: 600px) {
+@media only screen and (max-width: 600px) {
     .questions-input li {
-      width: 100%;
+        width: 100%;
     }
-
     .row-v-align {
-      flex-direction: row;
-      -webkit-flex-wrap: wrap;
-      -moz-flex-wrap: wrap;
-      -ms-flex-wrap: wrap;
-      -o-flex-wrap: wrap;
-      -khtml-flex-wrap: wrap;
-      flex-wrap: wrap;
+        flex-direction: row;
+        -webkit-flex-wrap: wrap;
+        -moz-flex-wrap: wrap;
+        -ms-flex-wrap: wrap;
+        -o-flex-wrap: wrap;
+        -khtml-flex-wrap: wrap;
+        flex-wrap: wrap;
     }
-  }
+}
 
 </style>
