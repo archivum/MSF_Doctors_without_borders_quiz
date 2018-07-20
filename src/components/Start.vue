ff1f27253b4b04030e59f20b1333eb97b58a7805
Igi <igi@candybanners.com>
2018-07-21 05:19:45 +0800

    scrollable off

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -90,6 +90,12 @@ help us tell the story of the people who need us. Thank you!<br><a href="http://
 
 </script>
 
+<style>
+body {
+  overflow: hidden !important;
+}
+</style>
+
 <style scoped>
 
 /*Links*/
