5fdaa2b75677f18866811a267e4bca6b96113f99
GuillaumeFerron <guillaume.ferron2@gmail.com>
2019-05-06 16:02:47 -0400

    Change error message

--- src/lib/copies.js
+++ src/lib/copies.js
@@ -1,111 +1,111 @@
 const copies = {
   en: {
     start:
-        {
-          line1: 'Take the doctors',
-          line2: 'without borders quiz',
-          line3: 'What kind of humanitarian',
-          line4: 'aid worker are you?',
-          cta: 'Start Quiz',
-          cta_info: 'Click for more information',
-          cta_info_mobile: 'Tap here for more information',
-          info_part1: 'Doctors Without Borders / Médecins Sans Frontières (MSF) humanitarian aid workers are compassionate, adventurous, and should be able to think on their feet. In addition, each of them has a unique perspective which make them well-suited for their particular roles.',
-          info_part2: 'What kind of humanitarian aid worker are you? Take our quick quiz to find out. It’s fun, it’s short, you’ll learn more about MSF, and you’ll find your fit in the work we do.',
-          info_num_of_q: {
-            part1: 'Questions:',
-            val: '5'
-          },
-          info_est_time: {
-            part1: 'Estimated Time:',
-            val: '2',
-            unit: 'min'
-          },
-          disclaimer: 'Your answers shed light on prevailing attitudes and preferences which in turn inform our fundraising decisions. People like you help us tell the story of the people who need us. Thank you!',
-          disclaimer_cta: 'View our Privacy Policy here.',
-          privacy_policy: 'http://www.doctorswithoutborders.ca/privacy-notice'
+      {
+        line1: 'Take the doctors',
+        line2: 'without borders quiz',
+        line3: 'What kind of humanitarian',
+        line4: 'aid worker are you?',
+        cta: 'Start Quiz',
+        cta_info: 'Click for more information',
+        cta_info_mobile: 'Tap here for more information',
+        info_part1: 'Doctors Without Borders / Médecins Sans Frontières (MSF) humanitarian aid workers are compassionate, adventurous, and should be able to think on their feet. In addition, each of them has a unique perspective which make them well-suited for their particular roles.',
+        info_part2: 'What kind of humanitarian aid worker are you? Take our quick quiz to find out. It’s fun, it’s short, you’ll learn more about MSF, and you’ll find your fit in the work we do.',
+        info_num_of_q: {
+          part1: 'Questions:',
+          val: '5'
         },
+        info_est_time: {
+          part1: 'Estimated Time:',
+          val: '2',
+          unit: 'min'
+        },
+        disclaimer: 'Your answers shed light on prevailing attitudes and preferences which in turn inform our fundraising decisions. People like you help us tell the story of the people who need us. Thank you!',
+        disclaimer_cta: 'View our Privacy Policy here.',
+        privacy_policy: 'http://www.doctorswithoutborders.ca/privacy-notice'
+      },
     quiz_form:
-        {
-          line1: 'Your results are almost in!',
-          line2: 'Will you stand with us? We need people like you to help us respond to crises around the world. Sign up now.',
-          check: 'Join our supporters so you\'ll be the first to know when a crisis occurs. MSF may contact you about different opportunities to help. You can unsubscribe at any time. Your privacy is important to us.',
-          check_link: 'Learn more here.',
-          check_phone_removed: 'Join our supporters so you\'ll be the first to know when a crisis occurs. MSF may contact you about different opportunities to help. You can unsubscribe at any time. Your privacy is important to us.',
-          continue: 'continue',
-          skip: 'Skip this step',
-          f_name: 'First Name*',
-          l_name: 'Last Name*',
-          email: 'Email*',
-          number: 'Phone Number*',
-          back: 'Back',
-          privacy_policy: 'http://www.doctorswithoutborders.ca/privacy-notice',
-          warning: '*Name and Email fields are required'
-        },
+      {
+        line1: 'Your results are almost in!',
+        line2: 'Will you stand with us? We need people like you to help us respond to crises around the world. Sign up now.',
+        check: 'Join our supporters so you\'ll be the first to know when a crisis occurs. MSF may contact you about different opportunities to help. You can unsubscribe at any time. Your privacy is important to us.',
+        check_link: 'Learn more here.',
+        check_phone_removed: 'Join our supporters so you\'ll be the first to know when a crisis occurs. MSF may contact you about different opportunities to help. You can unsubscribe at any time. Your privacy is important to us.',
+        continue: 'continue',
+        skip: 'Skip this step',
+        f_name: 'First Name*',
+        l_name: 'Last Name*',
+        email: 'Email*',
+        number: 'Phone Number*',
+        back: 'Back',
+        privacy_policy: 'http://www.doctorswithoutborders.ca/privacy-notice',
+        warning: '*All fields required'
+      },
     profile:
-        {
-          header: 'Your humanitarian aid worker profile is',
-          tell: 'Tell your friends you\'re',
-          share: 'Share Now',
-          saveShare: 'SAVE & SHARE',
-          copyLink: 'Copy Link',
-          copied: 'Copied !',
-          tweetShare: 'Twitter Share',
-          fbShare: 'Facebook Share'
-        }
+      {
+        header: 'Your humanitarian aid worker profile is',
+        tell: 'Tell your friends you\'re',
+        share: 'Share Now',
+        saveShare: 'SAVE & SHARE',
+        copyLink: 'Copy Link',
+        copied: 'Copied !',
+        tweetShare: 'Twitter Share',
+        fbShare: 'Facebook Share'
+      }
   },
   fr: {
     start:
-        {
-          line1: 'JEU-QUESTIONNAIRE DE',
-          line2: 'MÉDECINS SANS FRONTIÈRES',
-          line3: 'Quel type de travailleur',
-          line4: 'humanitaire êtes-vous ?',
-          cta: 'COMMENCEZ LE JEU-QUESTIONNAIRE',
-          cta_info: 'Cliquez ici pour plus d’informations',
-          cta_info_mobile: "Appuyez ici pour plus d'informations",
-          info_part1: "Les travailleurs humanitaires de Médecins Sans Frontières (MSF) / Doctors Without Borders sont pleins de compassion, ont soif d'aventure et doivent être réactifs. De plus, chacun d'entre eux a une perspective unique qui le rend bien adapté à son rôle.",
-          info_part2: "Quel est type de travailleur humanitaire êtes-vous ? Répondez à notre petit jeu-questionnaire pour le savoir. C'est amusant et rapide ! Puis, vous en apprendrez plus sur MSF, et vous pourrez imaginer votre place dans notre travail.",
-          info_num_of_q: {
-            part1: 'Nombre de questions :',
-            val: '5'
-          },
-          info_est_time: {
-            part1: 'Temps estimé :',
-            val: '2',
-            unit: 'min'
-          },
-          disclaimer: "Vos réponses nous aideront également à connaître les attitudes et préférences dominantes qui, à leur tour, éclaireront nos décisions de collecte de fonds. Vous nous aiderez à raconter plus efficacement l'histoire des personnes qui ont le plus besoin de nous. Merci !",
-          disclaimer_cta: 'Consultez notre politique de confidentialité ici.',
-          privacy_policy: 'http://www.medecinssansfrontieres.ca/avis-de-confidentialit%C3%A9'
+      {
+        line1: 'JEU-QUESTIONNAIRE DE',
+        line2: 'MÉDECINS SANS FRONTIÈRES',
+        line3: 'Quel type de travailleur',
+        line4: 'humanitaire êtes-vous ?',
+        cta: 'COMMENCEZ LE JEU-QUESTIONNAIRE',
+        cta_info: 'Cliquez ici pour plus d’informations',
+        cta_info_mobile: 'Appuyez ici pour plus d\'informations',
+        info_part1: 'Les travailleurs humanitaires de Médecins Sans Frontières (MSF) / Doctors Without Borders sont pleins de compassion, ont soif d\'aventure et doivent être réactifs. De plus, chacun d\'entre eux a une perspective unique qui le rend bien adapté à son rôle.',
+        info_part2: 'Quel est type de travailleur humanitaire êtes-vous ? Répondez à notre petit jeu-questionnaire pour le savoir. C\'est amusant et rapide ! Puis, vous en apprendrez plus sur MSF, et vous pourrez imaginer votre place dans notre travail.',
+        info_num_of_q: {
+          part1: 'Nombre de questions :',
+          val: '5'
         },
+        info_est_time: {
+          part1: 'Temps estimé :',
+          val: '2',
+          unit: 'min'
+        },
+        disclaimer: 'Vos réponses nous aideront également à connaître les attitudes et préférences dominantes qui, à leur tour, éclaireront nos décisions de collecte de fonds. Vous nous aiderez à raconter plus efficacement l\'histoire des personnes qui ont le plus besoin de nous. Merci !',
+        disclaimer_cta: 'Consultez notre politique de confidentialité ici.',
+        privacy_policy: 'http://www.medecinssansfrontieres.ca/avis-de-confidentialit%C3%A9'
+      },
     quiz_form:
-        {
-          line1: 'Vos résultats sont en cours de comptabilisation !',
-          line2: 'Serez-vous à nos côtés ? Nous avons besoin de gens comme vous qui prendront la parole lorsque le monde a besoin d\'actions concrètes. Inscrivez-vous dès maintenant.',
-          check: 'Inscrivez-vous à titre de supporteur et nous vous informerons dès qu\'une crise surviendra. MSF pourrait vous contacter pour vous informer des différentes façons dont vous pouvez aider. Vous pouvez vous désinscrire de la liste à tout moment. Nous accordons beaucoup d\'importance au respect de votre vie privée.',
-          check_link: 'Pour plus de détails, cliquez ici.',
-          check_phone_removed: 'Choisissez de rejoindre notre communauté d\'humanitaires. Vous pouvez vous désinscrire à tout moment. Nous accordons beaucoup d\'importance au respect de votre vie privée.',
-          continue: 'continuer',
-          skip: 'Passer cette étape',
-          f_name: 'Prénom*',
-          l_name: 'Nom de famille*',
-          email: 'Email*',
-          number: 'Numéro de téléphone*',
-          back: 'Précédent',
-          privacy_policy: 'http://www.medecinssansfrontieres.ca/avis-de-confidentialit%C3%A9',
-          warning: '*Nom et Courriel sont des champs obligatoires'
-        },
+      {
+        line1: 'Vos résultats sont en cours de comptabilisation !',
+        line2: 'Serez-vous à nos côtés ? Nous avons besoin de gens comme vous qui prendront la parole lorsque le monde a besoin d\'actions concrètes. Inscrivez-vous dès maintenant.',
+        check: 'Inscrivez-vous à titre de supporteur et nous vous informerons dès qu\'une crise surviendra. MSF pourrait vous contacter pour vous informer des différentes façons dont vous pouvez aider. Vous pouvez vous désinscrire de la liste à tout moment. Nous accordons beaucoup d\'importance au respect de votre vie privée.',
+        check_link: 'Pour plus de détails, cliquez ici.',
+        check_phone_removed: 'Choisissez de rejoindre notre communauté d\'humanitaires. Vous pouvez vous désinscrire à tout moment. Nous accordons beaucoup d\'importance au respect de votre vie privée.',
+        continue: 'continuer',
+        skip: 'Passer cette étape',
+        f_name: 'Prénom*',
+        l_name: 'Nom de famille*',
+        email: 'Email*',
+        number: 'Numéro de téléphone*',
+        back: 'Précédent',
+        privacy_policy: 'http://www.medecinssansfrontieres.ca/avis-de-confidentialit%C3%A9',
+        warning: '*Tous les champs sont obligatoires'
+      },
     profile:
-        {
-          header: 'Vous êtes un',
-          tell: 'Dites à vos amis que vous êtes',
-          share: 'PARTAGEZ MAINTENANT',
-          saveShare: 'SAUVER PARTAGER',
-          copyLink: 'Copier le lien',
-          copied: 'Copié !',
-          tweetShare: 'Partager sur Twitter',
-          fbShare: 'Partager sur Facebook'
-        }
+      {
+        header: 'Vous êtes un',
+        tell: 'Dites à vos amis que vous êtes',
+        share: 'PARTAGEZ MAINTENANT',
+        saveShare: 'SAUVER PARTAGER',
+        copyLink: 'Copier le lien',
+        copied: 'Copié !',
+        tweetShare: 'Partager sur Twitter',
+        fbShare: 'Partager sur Facebook'
+      }
   }
 }
 
